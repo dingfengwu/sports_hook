fb087e77e63cdb626812f8e867be7e22da9ef928
dingfengwu <dingfeng_wu@hotmail.com>
2021-10-14 11:39:38 +0800

    保存

--- common/js/util/errorCode.js
+++
@@ -1,9 +0,0 @@
-export const errorCode = {
-	captchaError: 8023,
-	loginTimeout: 3004,
-	invalidToken: 2020
-};
-export const isInvalidLogin = (code) => {
-	if (code === errorCode.loginTimeout || code === errorCode.invalidToken) return true;
-	return false;
-}
